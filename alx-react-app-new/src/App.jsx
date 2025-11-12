f6d661b58f0c8414af92fd8967f24a71f3c30d69
罗纳德·穆加博 <mugaboronald12@gmail.com>
2025-11-12 16:13:59 +0200

    feat: add simple counter app using useState hook

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
